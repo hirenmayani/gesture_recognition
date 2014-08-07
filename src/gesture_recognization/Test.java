da838c146fe1b4abc44d4cb338d29a49711a05d9
hirenmayani <mayanihiren@gmail.com>
2014-08-07 12:29:18 +0530

    1st project upload

---
+++ src/gesture_recognization/Test.java
@@ -0,0 +1,14 @@
+package gesture_recognization;
+
+import org.opencv.core.Core;
+import org.opencv.core.CvType;
+import org.opencv.core.Mat;
+ 
+public class Test {
+    public static void main(String[] args) {
+        System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
+        Mat m  = Mat.eye(7, 3, CvType.CV_8UC1);
+        System.out.println("m = " + m.dump());
+        
+    }
+}
