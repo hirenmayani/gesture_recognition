428ce6f8d5a5fe8ab807879f131cdea40295534c
Dhruvin <Dhruvin@Dhruvin-PC>
2014-08-14 02:33:39 +0530

    Jpanel inserted

--- src/gesture_recognition/FaceDetection.java
+++ src/gesture_recognition/FaceDetection.java
@@ -4,6 +4,10 @@ package gesture_recognition;
  * Detects face
  * External sources required XML file of configuration
  * */ 
+
+import jpanel_dispay.FacePanel;
+import image_converter.Convert;
+import java.awt.image.BufferedImage;
 import org.opencv.core.Core;
 import org.opencv.core.Mat;
 import org.opencv.core.MatOfRect;
@@ -13,45 +17,56 @@ import org.opencv.core.Scalar;
 import org.opencv.highgui.*;
 import org.opencv.objdetect.CascadeClassifier;
  
-public class FaceDetection {
-     
-	public static void main(String[] args) {
+public class FaceDetection
+{
+	public static void main(String[] args)
+	{
         System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
-        System.out.println("\nRunning FaceDetection");
-        new FaceDetection().faceDetector();
-    }
+       	new FaceDetection().faceDetector();   
+	}
      
     //Face Detection Method
-    void faceDetector() {
+    void faceDetector()
+    {
+    	int frameno = 0;
+    	FacePanel panel = new FacePanel();
+    	panel.initialize();
     	VideoCapture camera = new VideoCapture(0); //Opens the camera. 0 for in built camera
         Mat image = new Mat(); //creates matrix for image
+        BufferedImage buffimage = null;
         
         //Checks whether camera is opened or not
-        if(camera.isOpened()) {
-        	//while(true) {
+        if(camera.isOpened())
+        {
+        	while(true)
+            {
         		//Captures the image from camera
-        		if(camera.read(image)) {
-        			//Class for detecting face. Provided by OpenCV
-        			CascadeClassifier FaceDetection = new CascadeClassifier("other/lbpcascade_frontalface.xml");
- 
-        			MatOfRect faceDetections = new MatOfRect();
-        		
-        			//This Method detects faces
-        			FaceDetection.detectMultiScale(image, faceDetections);
- 
-        			System.out.println(String.format("Detected %s faces", faceDetections.toArray().length));
- 
-        			//Draw a green rectangle around face
-        			for (Rect rect : faceDetections.toArray()) {
-        				Core.rectangle(image, new Point(rect.x, rect.y), new Point(rect.x + rect.width, rect.y + rect.height),
-        						new Scalar(0, 255, 0));
+        		if(camera.read(image))
+        		{
+        			frameno++;
+        			if((frameno % 5) == 0)
+        			{
+        				//Class for detecting face. Provided by OpenCV
+        				CascadeClassifier FaceDetection = new CascadeClassifier("other/lbpcascade_frontalface.xml");
+    
+        				MatOfRect faceDetections = new MatOfRect();
+        			
+        				//This Method detects faces
+        				FaceDetection.detectMultiScale(image, faceDetections);
+     
+        				System.out.println(String.format("Detected %s faces", faceDetections.toArray().length));
+     
+        				//Draw a green rectangle around face
+        				for (Rect rect : faceDetections.toArray())
+        				{
+        					Core.rectangle(image, new Point(rect.x, rect.y), new Point(rect.x + rect.width, rect.y + rect.height),
+        							new Scalar(0, 255, 0));
+        				}
+        				buffimage = new Convert().matToBuff(image);
+        				panel.display(buffimage); 
         			}
- 
-        			String filename = "ouput.png";// give path of destination image
-        			System.out.println(String.format("Writing %s", filename));
-        			Highgui.imwrite(filename, image);
         		}
-        	}
+            }
         }
     }
-//}
+}
---
+++ src/jpanel_dispay/FacePanel.java
@@ -0,0 +1,57 @@
+package jpanel_dispay;
+
+import java.awt.Graphics;
+import java.awt.image.BufferedImage;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class FacePanel extends JPanel
+{
+	JFrame frame;
+	FacePanel face;
+	private static final long serialVersionUID = 1L;
+	private BufferedImage image;
+	int count = 0;
+	
+	public FacePanel()
+	{
+		super();
+	}
+	
+	public void setFace(BufferedImage img)
+	{
+		image = img;
+	}
+	
+	public void paintComponent(Graphics g)
+	{
+		super.paintComponent(g);
+		if(this.image == null)
+		{
+			return;
+		}
+		g.drawImage(this.image , 10 ,10 , this.image.getWidth() , this.image.getHeight() , null);
+		//g.setColor(Color.WHITE);
+		g.drawString("Frame no " + (count++),50,50);
+		
+	}
+	
+	public void initialize()
+    {
+    	frame = new JFrame();
+    	frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    	frame.setSize(500, 500);
+    	face = new FacePanel();
+    	frame.setContentPane(face);
+    	frame.setVisible(true);
+    }
+	
+    public void display(BufferedImage image)
+    {
+    	
+		face.setFace(image);
+		face.repaint();
+    }
+}
+
