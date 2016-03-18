a19071348ae4793ca0c26abbf1d22914f168d877
hirenmayani <mayanihiren@gmail.com>
2016-03-18 21:25:43 +0530

    pending

---
+++ src/gesture/detection/configuration/ConfigPropReader.java
@@ -0,0 +1,64 @@
+package gesture.detection.configuration;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+/**
+ * @author hirenmayani.com
+ * 
+ */
+
+public class ConfigPropReader {
+	String result = "";
+	static InputStream inputStream;
+	static Properties prop;
+	static String propFileName = null;
+
+	   private static ConfigPropReader instance = null;
+	   
+	   private ConfigPropReader() {
+	      // Exists only to defeat instantiation.
+	   }
+	   public static ConfigPropReader getInstance() {
+	      if(instance == null) {
+	         instance = new ConfigPropReader();
+	      }
+	      return instance;
+	   }
+
+	public static void init(String propFilePath) {
+		propFileName = propFilePath;
+		prop = new Properties();
+		FileInputStream inputStream;
+		try {
+			if (propFilePath == null)
+				inputStream = new FileInputStream("other/config.properties");
+			else
+				inputStream = new FileInputStream(propFilePath);
+
+			prop.load(inputStream);
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
+	}
+
+	public static void killit() {
+		try {
+			inputStream.close();
+		} catch (IOException e) {
+			System.out.println("error while closing");
+		}
+	}
+
+	public static String getPropValues(String key) {
+		try {
+		return prop.getProperty(key);
+		} catch (Exception e) {
+			System.out.println("Exception: " + e);
+		} finally {
+		}
+		return null;
+	}
+}
